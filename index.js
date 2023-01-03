8d82f913dd68f93358d7e6d02a80adc752bdea0c
[Norah Taylor] <emt368@gmail.com>
2023-01-03 15:03:58 -0500

    commit tree a99c066efc062a0011b7da19c890227efc0c70b6

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+// var customerName = "bob"
+// const leastFavoriteCustomer = "someValue"
+
+// //console.log(customerName.toUpperCase) 
+
+// function upperCaseCustomerName(){
+//     customerName = customerName.toUpperCase()
+//  }
+
+// function setBestCustomer(){
+//     bestCustomer = "not bob";
+// }
+
+// function overwriteBestCustomer() {
+//     bestCustomer = "maybe bob"
+// }
+
+// function changeLeastFavoriteCustomer(){
+//     leastFavoriteCustomer = "anotherValue"
+// }
+
+
+var customerName = "bob"
+var bestCustomer;
+
+function upperCaseCustomerName(){
+    customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+     bestCustomer = "not bob"
+}
